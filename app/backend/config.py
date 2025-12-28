499cd99c2271cbc0b6f4ebaad3d770f14d3a4f98
Pablo <pablogarciamolina6@gmail.com>
2025-12-28 19:58:48 +0100

    [UPDATE] Updated main and configuration for the extension of the backend

--- app/backend/config.py
+++ app/backend/config.py
@@ -2,3 +2,8 @@ import os
 
 BACKEND_PORT = int(os.getenv("BACKEND_PORT", 8000))
 MODEL_NAME = os.getenv("MODEL_NAME", "pretrained/base_model")
+
+GRADCAM_TARGET_LAYER = "features.11"
+GRADIENT_ASCENT_IMAGE_SIZE = (224, 224)
+ROAD_PERCENTILES = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+AVERAGE_SENSITIVITY_SAMPLES = 30
--- app/backend/main.py
+++ app/backend/main.py
@@ -5,10 +5,13 @@ Backend main
 from flask import Flask
 from .api.predict import predict_bp
 from .api.metrics import metrics_bp
+from .api.xai import xai_bp
 from .config import MODEL_NAME, BACKEND_PORT
 
 from src.config import BIG_CATS_PIPELINE_CONFIG
 from src.core.pipelines import load_model, Classifier_Pipeline, Pipeline_Config
+from src.core.models.alexnet import AlexNet
+from .utils.xai_service import XAI_Service
 
 def raise_backend() -> Flask:
     """
@@ -16,7 +19,7 @@ def raise_backend() -> Flask:
     """
     app = Flask(__name__)
 
-    print("🔄 Loading model...")
+    print("🔄 Loading model for prediction pipeline...")
     model, model_name = load_model(MODEL_NAME)
     print(f"✅ Model ({model_name}) loaded")
 
@@ -25,9 +28,20 @@ def raise_backend() -> Flask:
     app.config["pipeline"] = pipeline
     print("✅ Pipeline set up successfully")
 
+    print("🔄 Loading separate model for XAI service...")
+    model_xai = AlexNet(input_channels=3, num_classes=10)
+    model_xai.load_state_dict(model.state_dict())
+    print("✅ Separate model loaded for XAI")
+
+    print("🔄 Initializing XAI service...")
+    xai_service = XAI_Service(model_xai, device=str(BIG_CATS_PIPELINE_CONFIG["device"]))
+    app.config["xai_service"] = xai_service
+    print("✅ XAI service initialized")
+
     # Endpoints
     app.register_blueprint(predict_bp)
     app.register_blueprint(metrics_bp)
+    app.register_blueprint(xai_bp)
 
     return app
 
@@ -35,3 +49,4 @@ def raise_backend() -> Flask:
 if __name__ == "__main__":
     app = raise_backend()
     app.run(host="0.0.0.0", port=BACKEND_PORT)
+
