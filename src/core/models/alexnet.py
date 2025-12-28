[UPDATE] Corrected gradients inplace operations and hook issues

src/core/models/alexnet.py
@@ -15,21 +15,21 @@ class AlexNet(torch.nn.Module):
 
         self.features = torch.nn.Sequential(
             torch.nn.Conv2d(input_channels, 96, kernel_size=11, stride=4, padding=2),
-            torch.nn.ReLU(inplace=True),
+            torch.nn.ReLU(inplace=False),
             torch.nn.MaxPool2d(kernel_size=3, stride=2),
 
             torch.nn.Conv2d(96, 256, kernel_size=5, padding=2),
-            torch.nn.ReLU(inplace=True),
+            torch.nn.ReLU(inplace=False),
             torch.nn.MaxPool2d(kernel_size=3, stride=2),
 
             torch.nn.Conv2d(256, 384, kernel_size=3, padding=1),
-            torch.nn.ReLU(inplace=True),
+            torch.nn.ReLU(inplace=False),
 
             torch.nn.Conv2d(384, 384, kernel_size=3, padding=1),
-            torch.nn.ReLU(inplace=True),
+            torch.nn.ReLU(inplace=False),
 
             torch.nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            torch.nn.ReLU(inplace=True),
+            torch.nn.ReLU(inplace=False),
             torch.nn.MaxPool2d(kernel_size=3, stride=2),
         )
 
@@ -37,11 +37,11 @@ class AlexNet(torch.nn.Module):
             torch.nn.Flatten(1, -1),
 
             torch.nn.LazyLinear(4096),
-            torch.nn.ReLU(inplace=True),
+            torch.nn.ReLU(inplace=False),
             torch.nn.Dropout(),
 
             torch.nn.Linear(4096, 4096),
-            torch.nn.ReLU(inplace=True),
+            torch.nn.ReLU(inplace=False),
             torch.nn.Dropout(),
 
             torch.nn.Linear(4096, num_classes)