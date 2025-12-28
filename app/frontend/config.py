00ca261bc2ccb4edb8699a58b852c3fff6d99c88
Pablo <pablogarciamolina6@gmail.com>
2025-12-28 20:02:35 +0100

    [UPDATE] Updated frontend configuration

--- app/frontend/config.py
+++ app/frontend/config.py
@@ -3,3 +3,5 @@ import os
 IMAGES_PER_PAGE = 21
 MAX_GALLERY_COLUMNS = 7
 PREDICTION_BACKEND_ENDPOINT = os.getenv("PREDICTION_BACKEND_ENDPOINT", "http://localhost:8000/predict")
+LOCAL_XAI_ENDPOINT = os.getenv("LOCAL_XAI_ENDPOINT", "http://localhost:8000/xai/local")
+GLOBAL_XAI_ENDPOINT = os.getenv("GLOBAL_XAI_ENDPOINT", "http://localhost:8000/xai/global")
