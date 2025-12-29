ef95d337881d55cfde685c8a3accfb7f2bf7b0d1
Pablo <pablogarciamolina6@gmail.com>
2025-12-30 00:04:41 +0100

    [UPDATE] Updated data configuration

--- src/core/data/_config.py
+++ src/core/data/_config.py
@@ -14,8 +14,8 @@ BIG_CATS_TRAINING_TRANSFORMS = transforms.Compose([
     transforms.RandomCrop(BIG_CATS_IMAGE_SIZE),
     # RandomPatch(patch_size=(40, 40), probability=0.5, color=(0, 0, 0)),
     transforms.RandomHorizontalFlip(),
-    # transforms.ColorJitter(),
-    transforms.RandomInvert(0.3),
+    transforms.ColorJitter(),
+    # transforms.RandomInvert(0.3),
     transforms.RandomRotation(10),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
