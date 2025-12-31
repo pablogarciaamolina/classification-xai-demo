ce672afe3793676fe8661504a807f22597672bdf
Pablo <pablogarciamolina6@gmail.com>
2025-12-31 14:09:42 +0100

    [UPDATE] Corrected backend config

--- app/backend/config.py
+++ app/backend/config.py
@@ -5,7 +5,6 @@ from src.core.data.big_cats import Big_Cats_Dataset
 from src.config import STL10_PIPELINE_CONFIG, BIG_CATS_PIPELINE_CONFIG
 
 BACKEND_PORT = int(os.getenv("BACKEND_PORT", 8000))
-# MODEL_NAME = os.getenv("MODEL_NAME", "pretrained/STL10_ResNet34_base_model")
 
 GRADCAM_TARGET_LAYER = "processing_block.15.conv_branch.3"
 ROAD_PERCENTILES = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
